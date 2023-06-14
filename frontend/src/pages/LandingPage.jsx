5249dc7472c86de729ef858cf6225a4eb5a2cb14
Adrien Davy <adriendavyfx@gmail.com>
2023-06-15 00:13:49 +0200

    scss landing page and responsive done

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import particles from "../assets/img/particles.png";
+import cross from "../assets/img/cross.svg";
 
 function LandingPage() {
-  return <div className="landing-page-container">Landing Page</div>;
+  return (
+    <div className="landing-page-container">
+      <h1 className="welcome-title">Bienvenue sur Ciné Sparks</h1>
+      <main className="create-account-or-login">
+        <figure>
+          <div className="circle">
+            <img src={cross} alt="cross" className="cross" />
+          </div>
+          <figcaption>Créer un compte</figcaption>
+        </figure>
+        <figure>
+          <img src={particles} alt="cross" className="avatar-picture" />
+
+          <figcaption>Se Connecter</figcaption>
+        </figure>
+      </main>
+      <img src={particles} alt="Particles" className="particles" />
+    </div>
+  );
 }
 
 export default LandingPage;
