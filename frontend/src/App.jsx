3088e91276228ebe4021942f9efea174cc767e82
Adrien Davy <adriendavyfx@gmail.com>
2023-06-14 22:25:07 +0200

    setup sass

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import "./assets/styles/index.css";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import { UserProvider } from "./contexts/UserContext";
