f03bf1445ac4540dca616b8cd0d3d8a9f87c2bc1
Adrien Davy <adriendavyfx@gmail.com>
2023-06-14 18:06:50 +0200

    route find video and scenario by user

--- backend/src/routes/scenario.route.js
+++ backend/src/routes/scenario.route.js
@@ -3,6 +3,7 @@ const scenarioController = require("../controllers/scenarioController");
 
 router.get("/", scenarioController.browse);
 router.get("/genre", scenarioController.browseGenre);
+router.get("/user/:id", scenarioController.readUser);
 router.get("/:id", scenarioController.read);
 router.post("/", scenarioController.add);
 
